06fca99bd3baa1f1ada2ca12dfb4bd2b74255b9b
Avishai Ish-Shalom <aishshalom@outbrain.com>
2016-01-23 22:45:51 +0200

    Add list node subcommand

--- list.go
+++ list.go
@@ -20,6 +20,7 @@ func listRegisterCli(app *kingpin.Application, opts *appOpts) {
 	list.Flag("regex", "Filter by regular expression").Short('r').StringVar(&l.filterExp)
 	list.Command("service", "List Consult services").Action(l.listServiceHandler)
 	list.Command("dc", "List Consul DataCenters").Action(l.listDCHandler)
+	list.Command("node", "List Consul nodes").Action(l.listNodeHandler)
 }
 
 func (l *listCommand) listServiceHandler(context *kingpin.ParseContext) error {
@@ -69,4 +70,41 @@ func (l *listCommand) listDCs() ([]string, error) {
 	}
 
 	return dcs, nil
+}
+
+func (l *listCommand) listNodeHandler(context *kingpin.ParseContext) error {
+	if results, err := l.listNodes(); err != nil {
+		return err
+	} else {
+		short := make([]string, 0)
+		long := make([]string, 2)
+		long[0] = "Datacenter\tNode\tAddress"
+		long[1] = ""
+
+		for dc, nodes := range results {
+			for _, node := range nodes {
+				long = append(long, dc+"\t"+node.Node+"\t"+node.Address)
+				short = append(short, dc+"\t"+node.Node)
+			}
+		}
+
+		l.Output(results, long, short)
+		return nil
+	}
+}
+
+func (l *listCommand) listNodes() (map[string][]*api.Node, error) {
+	if clients, err := l.GetConsulClients(); err != nil {
+		return nil, err
+	} else {
+		results := make(map[string][]*api.Node)
+		for dc, client := range clients {
+			if nodes, _, err := client.Catalog().Nodes(&api.QueryOptions{}); err != nil {
+				return nil, err
+			} else {
+				results[dc] = nodes
+			}
+		}
+		return results, nil
+	}
 }
